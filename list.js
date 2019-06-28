9c3d846f1b8edcafb770c32d513d5611e9c8d868
11guangbai <1572606408@qq.com>
2019-06-28 20:38:24 +0800

    33

--- list.js
+++ list.js
@@ -7,7 +7,7 @@ var i = 0;
 function changeImg() {
 
     i++
-    document.getElementById("img1").src = "./img/" + i + ".jpg";
+    document.getElementById("img1").src = "../bbl/content/image/hong/" + i + ".jpg";
     if (i == 6) {
         i = 0;
     }
